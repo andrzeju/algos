7d8fbda3c9cc991d62c4f9008000d16b39113e17
Andrzej <andrzej.urban.pl@gmail.com>
2021-02-17 08:58:56 +0100

    Reverse int more readable solution

--- src/main/java/pl/com/urbanlab/algos101/InvertedIntDigits.java
+++ src/main/java/pl/com/urbanlab/algos101/InvertedIntDigits.java
@@ -5,25 +5,24 @@ import static org.junit.Assert.assertEquals;
 public class InvertedIntDigits {
 
     public static int reverse(int x) {
-        String stringifiedInt = Integer.toString(x);
-        StringBuilder reverseStringifiedInt = new StringBuilder();
-        int lastCharAt = stringifiedInt.length() - 1;
 
-        for (int i = 0; i < stringifiedInt.length(); i++) {
-            reverseStringifiedInt.append(stringifiedInt.charAt(lastCharAt - i));
-        }
-
-        if (x<0) {
-            reverseStringifiedInt.deleteCharAt(reverseStringifiedInt.length() - 1);
-            reverseStringifiedInt.insert(0,"-");
-        }
-
-        long tempIntStore = Long.parseLong(reverseStringifiedInt.toString()); //assuming that smaller than max long
-        if (tempIntStore > Integer.MAX_VALUE || tempIntStore < Integer.MIN_VALUE) {
+        if (x == Integer.MIN_VALUE) { //Math.abs doesn't work for MIN_VALUE
             return 0;
         }
 
-        return Integer.parseInt(reverseStringifiedInt.toString());
+        StringBuilder stringifiedInt = new StringBuilder(Integer.toString(Math.abs(x)));
+        stringifiedInt.reverse();
+
+        if (x<0) {
+            stringifiedInt.insert(0, "-");
+        }
+
+        long result = Long.parseLong(stringifiedInt.toString());
+        if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+            return 0;
+        }
+
+        return (int) result;
 
     }
 
@@ -31,6 +30,7 @@ public class InvertedIntDigits {
         assertEquals("int digits are reverted", 4321, reverse(1234));
         assertEquals("zero when over max value", 0, reverse(2_147_483_647));
         assertEquals("zero when under min value", 0, reverse(-2_147_483_648));
+        assertEquals("zero when under min value", 0, reverse(-2_147_483_647));
         assertEquals("zero when under min value", -543, reverse(-345));
     }
 }
