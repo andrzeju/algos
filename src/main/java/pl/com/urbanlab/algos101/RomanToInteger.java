1bd167061c25316d1b3fc4c8c0b33e042f228279
Andrzej <andrzej.urban.pl@gmail.com>
2021-02-24 07:09:33 +0100

    Hashmap solution

--- src/main/java/pl/com/urbanlab/algos101/RomanToInteger.java
+++ src/main/java/pl/com/urbanlab/algos101/RomanToInteger.java
@@ -2,10 +2,49 @@ package pl.com.urbanlab.algos101;
 
 import org.junit.Assert;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanToInteger {
 
+    static Map<String, Integer> mapping = new HashMap<String, Integer>(){{
+      put("I", 1);
+      put("V", 5);
+      put("X", 10);
+      put("L", 50);
+      put("C", 100);
+      put("D", 500);
+      put("M", 1000);
+      put("IV", 4);
+      put("IX", 9);
+      put("XL", 40);
+      put("XC", 90);
+      put("CD", 400);
+      put("CM", 900);
+    }};
+
+    //Map<String, Integer> mappingSub = new HashMap<String, Integer>(){{
+    //}};
+
     public static int romanToInt(String s) {
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+
+            boolean isLastChar = (i == s.length() - 1);
+
+            if (isLastChar) {
+                return sum + mapping.get(s.substring(i, i + 1));
+            }
+            String numberLiteral = s.substring(i, i + 2);
+
+            if (mapping.containsKey(numberLiteral)) {
+                sum += mapping.get(numberLiteral);
+                i++;
+            } else {
+                sum += mapping.get(s.substring(i, i + 1));
+            }
+        }
+        return sum;
     }
 
 
