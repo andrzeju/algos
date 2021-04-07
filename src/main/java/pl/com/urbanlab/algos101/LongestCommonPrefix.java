d5d8eccbb728b6754d28dc5fc35d4f32348fbb0e
Andrzej <andrzej.urban.pl@gmail.com>
2021-04-07 09:14:02 +0200

    common prefix - working solution

--- src/main/java/pl/com/urbanlab/algos101/LongestCommonPrefix.java
+++ src/main/java/pl/com/urbanlab/algos101/LongestCommonPrefix.java
@@ -5,7 +5,19 @@ import org.junit.Assert;
 public class LongestCommonPrefix {
 
     public static String getLongestPrefix(String [] words) {
-        return null;
+        StringBuilder commonPrefix = new StringBuilder();
+        if (words.length == 0) {
+            return "";
+        }
+        for (int i = 0; i < words[0].length(); i++) {
+            for (int j = 0; j < words.length - 1; j++) {
+                if (words[j+1].length() <= i || words[j].charAt(i) != words[j+1].charAt(i)) {
+                    return commonPrefix.toString();
+                }
+            }
+            commonPrefix.append(words[0].charAt(i));
+        }
+        return commonPrefix.toString();
     }
 
     public static void main(String[] args) {
@@ -13,5 +25,15 @@ public class LongestCommonPrefix {
         Assert.assertEquals("fl", getLongestPrefix(flStrings));
         Assert.assertNotEquals("fli", getLongestPrefix(flStrings));
         Assert.assertNotEquals("f", getLongestPrefix(flStrings));
+
+        String [] abArray = {"ab", "a"};
+        Assert.assertEquals("a", getLongestPrefix(abArray));
+
+        String [] emptyStrings = {"float", "fling", "boat"};
+        Assert.assertEquals("", getLongestPrefix(emptyStrings));
+
+        String [] emptyArray = {};
+        Assert.assertEquals("", getLongestPrefix(emptyArray));
+
     }
 }
