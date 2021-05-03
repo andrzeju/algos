c0d145343a838d924f2b6a9e4a94a4686221035a
Andrzej <andrzej.urban.pl@gmail.com>
2021-05-03 21:30:48 +0200

    valid parenthesis - basic solution

--- src/main/java/pl/com/urbanlab/algos101/ValidParenthesis.java
+++ src/main/java/pl/com/urbanlab/algos101/ValidParenthesis.java
@@ -2,20 +2,40 @@ package pl.com.urbanlab.algos101;
 
 import org.junit.Assert;
 
+import java.util.Map;
+
 public class ValidParenthesis {
 
     public static boolean isValid(String s) {
-        return false;
+        StringBuilder parenthesisStack = new StringBuilder();
+        String opening = "{[(";
+        Map<String, String> pairs = Map.ofEntries(
+                Map.entry("}", "{"),
+                Map.entry("]", "["),
+                Map.entry(")", "(")
+        );
+        for (int i = 0; i < s.length(); i++) {
+            String currentChar = String.valueOf(s.charAt(i));
+            if (opening.contains(currentChar)) {
+                parenthesisStack.append(s.charAt(i));
+            } else if (pairs.get(currentChar) != null && parenthesisStack.lastIndexOf(pairs.get(currentChar)) == parenthesisStack.length() -1){
+                parenthesisStack.deleteCharAt(parenthesisStack.length() -1);
+            } else {
+                return false;
+            }
+        }
+        return parenthesisStack.length() == 0;
     }
 
     public static void main(String[] args) {
         String valid1 = "[]";
         String valid2 = "{}()[]";
-        String valid3 = "[{()}]";
+        String valid3 = "[{()}]()";
         Assert.assertTrue("Single parentiesis are valid", isValid(valid1));
         Assert.assertTrue("Multiple parentiesis are valid", isValid(valid2));
         Assert.assertTrue("Nested parentiesis are valid", isValid(valid3));
         String invalid1 = "[}";
-        Assert.assertFalse("Mixed parentiesis are invalid", isValid(invalid1));
+        String invalid2 = "[43dfdfg}";
+        Assert.assertFalse("Invalid chars are invalid", isValid(invalid2));
     }
 }
