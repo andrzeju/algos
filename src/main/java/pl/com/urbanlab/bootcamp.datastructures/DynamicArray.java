9fe82284092e00d9b07b87ef4d62e8b2c7274f19
Andrzej <andrzej.urban.pl@gmail.com>
2021-05-04 11:46:22 +0200

    Dynamic array implementation - tests + empty class

---
+++ src/main/java/pl/com/urbanlab/bootcamp.datastructures/DynamicArray.java
@@ -0,0 +1,39 @@
+package pl.com.urbanlab.bootcamp.datastructures;
+
+public class DynamicArray<S> {
+
+    Object [] array;
+
+    public DynamicArray(int size) {
+        array = new Object[size];
+    }
+
+    public void set(int i, String a) {
+
+    }
+
+    public String get(int i) {
+        return null;
+    }
+
+    public void add(String a) {
+    }
+
+    public void insert(int i, String d) {
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void delete(int i) {
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean contains(String a) {
+        return false;
+    }
+}
---
+++ src/test/java/pl/com/urbanlab/algos101/bootcamp/datastructures/DynamicArrayTest.java
@@ -0,0 +1,107 @@
+package pl.com.urbanlab.algos101.bootcamp.datastructures;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import pl.com.urbanlab.bootcamp.datastructures.DynamicArray;
+
+public class DynamicArrayTest {
+
+    private DynamicArray<String> array;
+
+    @Before
+    public void setUp() {
+        array = new DynamicArray<String>(2);
+    }
+
+    @Test
+    public void getAndSet() {
+        array.set(0, "a");
+        Assert.assertEquals("a", array.get(0));
+    }
+
+    @Test
+    public void insert() {
+        array.add("a"); // 0
+        array.add("b"); // 1
+        array.add("c"); // 2
+
+        array.insert(1, "d");
+
+        Assert.assertEquals(4, array.size());
+        Assert.assertEquals("a", array.get(0));
+        Assert.assertEquals("d", array.get(1));
+        Assert.assertEquals("b", array.get(2));
+        Assert.assertEquals("c", array.get(3));
+    }
+
+    @Test
+    public void deleteFirst() {
+        array.add("a");
+        array.add("b");
+        array.add("c");
+
+        array.delete(0);
+
+        Assert.assertEquals(2, array.size());
+        Assert.assertEquals("b", array.get(0));
+        Assert.assertEquals("c", array.get(1));
+        Assert.assertEquals(null, array.get(2));
+    }
+
+    @Test
+    public void deleteMiddle() {
+        array.add("a");
+        array.add("b");
+        array.add("c");
+
+        array.delete(1);
+
+        Assert.assertEquals(2, array.size());
+        Assert.assertEquals("a", array.get(0));
+        Assert.assertEquals("c", array.get(1));
+        Assert.assertEquals(null, array.get(2));
+    }
+
+    @Test
+    public void deleteLast() {
+        array.add("a");
+        array.add("b");
+        array.add("c");
+
+        array.delete(2);
+
+        Assert.assertEquals(2, array.size());
+        Assert.assertEquals("a", array.get(0));
+        Assert.assertEquals("b", array.get(1));
+        Assert.assertEquals(null, array.get(2));
+    }
+
+    @Test
+    public void isEmpty() {
+        Assert.assertTrue(array.isEmpty());
+        array.add("a");
+        Assert.assertFalse(array.isEmpty());
+    }
+
+    @Test
+    public void contains()  {
+        Assert.assertFalse(array.contains("a"));
+        array.add("a");
+        Assert.assertTrue(array.contains("a"));
+        array.add("b");
+        array.add("b");
+        array.add("c");
+        Assert.assertTrue(array.contains("b"));
+        Assert.assertTrue(array.contains("c"));
+        array.delete(3);
+        Assert.assertFalse(array.contains("c"));
+        array.delete(2);
+        Assert.assertTrue(array.contains("b"));
+        array.delete(1);
+        Assert.assertFalse(array.contains("b"));
+        array.delete(0);
+        Assert.assertFalse(array.contains("a"));
+    }
+
+}
