a792a70302617b8d31b3a8a62fd43102f1ae257b
Andrzej <andrzej.urban.pl@gmail.com>
2021-10-19 17:35:38 +0200

    Stack unit test and empty class

---
+++ src/main/java/pl/com/urbanlab/bootcamp.datastructures/Stack.java
@@ -0,0 +1,4 @@
+package pl.com.urbanlab.bootcamp.datastructures;
+
+public class Stack {
+}
---
+++ src/test/java/pl/com/urbanlab/algos101/bootcamp/datastructures/StackTest.java
@@ -0,0 +1,31 @@
+package pl.com.urbanlab.algos101.bootcamp.datastructures;
+
+import org.junit.Before;
+import org.junit.Test;
+import pl.com.urbanlab.bootcamp.datastructures.Stack;
+
+public class StackTest {
+
+    private Stack stack;
+
+    @Before
+    public void setup() {
+        stack = new Stack();
+    }
+
+    @Test
+    public void push() {
+
+    }
+
+    @Test
+    public void pop() {
+
+    }
+
+    @Test
+    public void peek() {
+
+    }
+
+}
