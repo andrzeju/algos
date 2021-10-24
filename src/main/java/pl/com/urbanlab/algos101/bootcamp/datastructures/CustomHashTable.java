f180615ef24e087930c68282990174c3385b4d5a
Andrzej <andrzej.urban.pl@gmail.com>
2021-10-24 11:01:07 +0200

    full implementation with collision handling

--- src/main/java/pl/com/urbanlab/algos101/bootcamp/datastructures/CustomHashTable.java
+++ src/main/java/pl/com/urbanlab/algos101/bootcamp/datastructures/CustomHashTable.java
@@ -2,11 +2,57 @@ package pl.com.urbanlab.algos101.bootcamp.datastructures;
 
 public class CustomHashTable {
 
-    public void put(String key, String value) {
+    private static final int INITIAL_TABLE_SIZE = 8;
+    private HashEntry [] entries;
 
+    public CustomHashTable() {
+        entries = new HashEntry[INITIAL_TABLE_SIZE];
     }
 
+    public void put(String key, String value) {
+        int index = getIndex(key);
+        System.out.println("Inserting: idx=" + index + ", key=" + key + ", val=" + value);
+        HashEntry newEntry = new HashEntry(key, value);
+
+        if (entries[index] == null) {
+            entries[index] = newEntry;
+        } else {
+            HashEntry entry = entries[index];
+            while (entry.next != null) {
+                entry = entry.next;
+            }
+            entry.next = newEntry;
+        }
+    }
+
+
     public String get(String key) {
-        return null;
+        int index = getIndex(key);
+
+        if (entries[index] == null) {
+            return null;
+        }
+        HashEntry currentEntry = entries[index];
+
+        while (!currentEntry.key.equals(key)) {
+            currentEntry = currentEntry.next;
+        }
+        return currentEntry.value;
+    }
+
+    private int getIndex(String key) {
+        return Math.abs(key.hashCode()) % INITIAL_TABLE_SIZE;
+    }
+
+    private class HashEntry {
+        private final String key;
+        private final String value;
+        private HashEntry next;
+
+        private HashEntry(String key, String value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
+        }
     }
 }
--- src/test/java/pl/com/urbanlab/algos101/bootcamp/datastructures/CustomHashTableTest.java
+++ src/test/java/pl/com/urbanlab/algos101/bootcamp/datastructures/CustomHashTableTest.java
@@ -15,11 +15,36 @@ public class CustomHashTableTest {
     }
 
     @Test
-    public void putAndGet() {
+    public void testPutAndGet() {
         hashTable.put("John Wayne", "555-33-22");
         hashTable.put("Jane Doe", "555-22-11");
         assertEquals("555-33-22", hashTable.get("John Wayne"));
         assertEquals("555-22-11", hashTable.get("Jane Doe"));
+    }
+
+    @Test
+    public void testCollisionByProvidingMoreEntriesThanTableSize() {
+        hashTable.put("Jane Doe0", "555-22-10");
+        hashTable.put("Jane Doe1", "555-22-11");
+        hashTable.put("Jane Doe2", "555-22-12");
+        hashTable.put("Jane Doe3", "555-22-13");
+        hashTable.put("Jane Doe4", "555-22-14");
+        hashTable.put("Jane Doe5", "555-22-15");
+        hashTable.put("Jane Doe6", "555-22-16");
+        hashTable.put("Jane Doe7", "555-22-17");
+        hashTable.put("Jane Doe8", "555-22-18");
+        hashTable.put("Jane Doe9", "555-22-19");
+
+        assertEquals("555-22-10", hashTable.get("Jane Doe0"));
+        assertEquals("555-22-11", hashTable.get("Jane Doe1"));
+        assertEquals("555-22-12", hashTable.get("Jane Doe2"));
+        assertEquals("555-22-13", hashTable.get("Jane Doe3"));
+        assertEquals("555-22-14", hashTable.get("Jane Doe4"));
+        assertEquals("555-22-15", hashTable.get("Jane Doe5"));
+        assertEquals("555-22-16", hashTable.get("Jane Doe6"));
+        assertEquals("555-22-17", hashTable.get("Jane Doe7"));
+        assertEquals("555-22-18", hashTable.get("Jane Doe8"));
+        assertEquals("555-22-19", hashTable.get("Jane Doe9"));
     }
 
 }
