82140e35e5c3bb69626740e945f807ba99375623
Andrzej <andrzej.urban.pl@gmail.com>
2021-10-24 12:48:26 +0200

    binary tree insert, find and min implementation

--- src/main/java/pl/com/urbanlab/bootcamp.datastructures/CustomBinaryTree.java
+++ src/main/java/pl/com/urbanlab/bootcamp.datastructures/CustomBinaryTree.java
@@ -1,4 +1,64 @@
 package pl.com.urbanlab.bootcamp.datastructures;
 
 public class CustomBinaryTree {
+
+    private CustomTreeNode data;
+
+    public void insert(int key, String value) {
+        data = putRecursively(key, value, data);
+    }
+
+    private CustomTreeNode putRecursively(int key, String value, CustomTreeNode node) {
+        if (node == null) {
+            node = new CustomTreeNode(key, value);
+        } else if (key > node.key) {
+            node.right = putRecursively(key, value, node.right);
+        } else {
+            node.left = putRecursively(key, value, node.left);
+        }
+        return node;
+    }
+
+    public String find(int key) {
+        return findRecursively(key, data);
+    }
+
+    private String findRecursively(int key, CustomTreeNode node) {
+        String value;
+        if (node.key == key) {
+            value = node.value;
+        } else if (key > node.key) {
+            value = findRecursively(key, node.right);
+        } else {
+            value = findRecursively(key, node.left);
+        }
+        return value;
+    }
+
+    public String findMinimum() {
+        if (data == null) {
+            throw new IllegalStateException("No elements in the tree");
+        }
+
+        return data.min();
+    }
+
+    private class CustomTreeNode {
+        private final int key;
+        private final String value;
+        private CustomTreeNode left, right;
+
+        public CustomTreeNode(int key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        private String min() {
+            if (this.left == null) {
+                return this.value;
+            } else {
+                return this.left.min();
+            }
+        }
+    }
 }
--- src/test/java/pl/com/urbanlab/bootcamp/datastructures/BinaryTreeTest.java
+++ src/test/java/pl/com/urbanlab/bootcamp/datastructures/BinaryTreeTest.java
@@ -1,17 +1,42 @@
 package pl.com.urbanlab.bootcamp.datastructures;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class BinaryTreeTest {
 
     CustomBinaryTree tree;
 
+    @Before
     public void setup() {
-
+        tree = new CustomBinaryTree();
     }
 
     @Test
-    public void testPutAndGet() {
+    public void testInsertAndFind() {
+        tree.insert(1, "Bejing");
+        tree.insert(3, "New York");
+        tree.insert(5, "London");
+        tree.insert(2, "Dehli");
+        tree.insert(0, "Sosnowiec");
+
+        assertEquals("Bejing", tree.find(1));
+        assertEquals("New York", tree.find(3));
+        assertEquals("London", tree.find(5));
+        assertEquals("Dehli", tree.find(2));
+    }
+
+    @Test
+    public void testMinimum() {
+        tree.insert(1, "Bejing");
+        tree.insert(3, "New York");
+        tree.insert(5, "London");
+        tree.insert(2, "Dehli");
+        tree.insert(0, "Sosnowiec");
+
+        assertEquals("Sosnowiec", tree.findMinimum());
 
     }
 
