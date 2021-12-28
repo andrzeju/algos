1507db2baa8dbf8b6a9a4d42f50835ed5233b5e9
Andrzej <andrzej.urban.pl@gmail.com>
2021-12-28 16:16:36 +0100

    memoisation optimised fibo algorythm and test

--- src/main/java/pl/com/urbanlab/bootcamp.datastructures/FibonacciOptimisation.java
+++ src/main/java/pl/com/urbanlab/bootcamp.datastructures/FibonacciOptimisation.java
@@ -2,12 +2,32 @@ package pl.com.urbanlab.bootcamp.datastructures;
 
 public class FibonacciOptimisation {
 
+    private final Integer [] memoizedSequence = new Integer[1000];
+
     public int runStandardAlgo(int n) {
         if (n <= 0) {
             return 0;
         } else if (n == 1) {
             return 1;
-        } else
+        } else {
             return runStandardAlgo(n - 1) + runStandardAlgo(n - 2);
+        }
+    }
+
+    public int runOptimizedAlgo(int n) {
+        int result;
+        if (n <= 0) {
+            result =  0;
+        } else if (n == 1) {
+            result = 1;
+        } else {
+            Integer calculatedTwoBefore = memoizedSequence[n - 2];
+            Integer calculatedOneBefore = memoizedSequence[n - 1];
+            result = (calculatedOneBefore != null ? calculatedOneBefore : runOptimizedAlgo(n - 1))
+                    + (calculatedTwoBefore != null ? calculatedTwoBefore : runOptimizedAlgo(n - 2));
+        }
+
+        memoizedSequence[n] = result;
+        return result;
     }
 }
--- src/test/java/pl/com/urbanlab/bootcamp/datastructures/FibonacciOptimisationTest.java
+++ src/test/java/pl/com/urbanlab/bootcamp/datastructures/FibonacciOptimisationTest.java
@@ -22,4 +22,21 @@ public class FibonacciOptimisationTest {
 
     }
 
+    @Test
+    public void testOptimisedFiboSpeed() {
+        FibonacciOptimisation fiboOptimised = new FibonacciOptimisation();
+
+        long startTime = System.nanoTime();
+        Assert.assertEquals("10th fibo", 55, fiboOptimised.runOptimizedAlgo(10));
+        long endTime = System.nanoTime();
+        System.out.println("Time taken:" + (endTime - startTime));
+
+
+        startTime = System.currentTimeMillis();
+        Assert.assertEquals("20th fibo", 1134903170, fiboOptimised.runOptimizedAlgo(45));
+        endTime = System.currentTimeMillis();
+        System.out.println("Time taken [ms]:" + (endTime - startTime));
+
+    }
+
 }
