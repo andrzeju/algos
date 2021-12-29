ae5c0e495eee398dc71f36f89c9b90d05072cb54
Andrzej <andrzej.urban.pl@gmail.com>
2021-12-29 08:34:57 +0100

    characters trimmer with tests - setup method

--- src/test/java/pl/com/urbanlab/bootcamp/tasks/CharacterTrimmerTest.java
+++ src/test/java/pl/com/urbanlab/bootcamp/tasks/CharacterTrimmerTest.java
@@ -1,13 +1,20 @@
 package pl.com.urbanlab.bootcamp.tasks;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CharacterTrimmerTest {
 
+    private CharacterTrimmer trimmer;
+
+    @Before
+    public void setup() {
+         trimmer = new CharacterTrimmer();
+    }
+
     @Test
     public void testLeadingCharactersTrimmed() {
-        CharacterTrimmer trimmer = new CharacterTrimmer();
         Assert.assertEquals("123", trimmer.trim("123", "0"));
         Assert.assertEquals("123", trimmer.trim("0123", "0"));
         Assert.assertEquals("123", trimmer.trim("0000123", "0"));
@@ -15,7 +22,6 @@ public class CharacterTrimmerTest {
 
     @Test
     public void testTailingCharactersTrimmed() {
-        CharacterTrimmer trimmer = new CharacterTrimmer();
         Assert.assertEquals("123", trimmer.trim("1230", "0"));
         Assert.assertEquals("123", trimmer.trim("1230000", "0"));
         Assert.assertEquals("abc", trimmer.trim("xxabcxxx", "x"));
@@ -23,7 +29,6 @@ public class CharacterTrimmerTest {
 
     @Test
     public void testBothSidesCharactersTrimmed() {
-        CharacterTrimmer trimmer = new CharacterTrimmer();
         Assert.assertEquals("abc", trimmer.trim("xxabcxxx", "x"));
     }
 }
